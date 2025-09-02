79bf0bfe06fab33edd8fcf918c407c509b61aeda
lifeutsav <sutsav585@gmail.com>
2025-09-03 00:29:33 +0530

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,8 @@
+A = int(input("Enter the first number : "))
+B = int(input("Enter the Second number : "))
+
+
+print("Addition : ",A+B)
+print("Subtraction : ",A-B)
+print("Multiplication: ",A*B)
+print("Division : ",A/B)
